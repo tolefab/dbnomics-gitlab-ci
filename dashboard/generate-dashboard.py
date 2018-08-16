20123ebe394bbd13485f74dccd318ca739c0cf59
Christophe Benz <christophe.benz@cepremap.org>
2018-08-16 11:38:24 +0200

    Dashboard: add links when no jobs, display generation duration

--- dashboard/generate-dashboard.py
+++ dashboard/generate-dashboard.py
@@ -37,6 +37,7 @@ import logging
 import os
 import re
 import sys
+import time
 from io import StringIO
 from pathlib import Path
 from string import Template
@@ -89,24 +90,25 @@ def get_importer_job_variables(job):
 
 
 def print_html_dashboard(fetchers_projects, importer_project, pipeline_schedule_by_fetcher_id, jobs_by_fetcher_id,
-                         index_jobs_by_provider_slug):
+                         index_jobs_by_provider_slug, start_time):
     tbody_io = StringIO()
-    for project in fetchers_projects:
+    for provider_number, project in enumerate(fetchers_projects, start=1):
         provider_slug = project.name[:-len("-fetcher")]
         pipeline_schedule = pipeline_schedule_by_fetcher_id.get(project.id)
         fetcher_jobs = jobs_by_fetcher_id[project.id]
         download_jobs = list(take(3, fetcher_jobs["download"]))
         convert_jobs = list(take(3, fetcher_jobs["convert"]))
         index_jobs = index_jobs_by_provider_slug.get(provider_slug)
-        tbody_io.write(format_fetcher_tr(project, importer_project, provider_slug, pipeline_schedule,
+        tbody_io.write(format_fetcher_tr(project, importer_project, provider_number, provider_slug, pipeline_schedule,
                                          download_jobs, convert_jobs, index_jobs))
 
     dashboard_template = Template((script_dir / "dashboard.template.html").read_text())  # pylint: disable=E1101
     tbody_io.seek(0)
     tbody = tbody_io.read()
-    generation_date_str = "{:%c}".format(datetime.datetime.now())
+    generation_message = "Dashboard generated on {:%c} in {:.2f} seconds".format(
+        datetime.datetime.now(), time.time() - start_time)
     print(dashboard_template.substitute(
-        generation_date_str=generation_date_str,
+        generation_message=generation_message,
         tbody=tbody,
     ))
 
@@ -149,17 +151,36 @@ def format_pipeline_schedule_link(project, pipeline_schedule):
     return pipeline_schedule_link
 
 
-def format_fetcher_tr(project, importer_project, provider_slug, pipeline_schedule,
+def format_fetcher_tr(project, importer_project, provider_number, provider_slug, pipeline_schedule,
                       download_jobs, convert_jobs, index_jobs):
     pipeline_schedule_link = format_pipeline_schedule_link(project, pipeline_schedule)
-    download_links = "".join(
-        format_job_link(project, job)
-        for job in download_jobs
-    )
-    conversion_links = "".join(
-        format_job_link(project, job)
-        for job in convert_jobs
-    )
+
+    fetcher_jobs_url = "{}/{}/-/jobs".format(args.gitlab_base_url, project.path_with_namespace)
+
+    if download_jobs:
+        download_links = "".join(
+            format_job_link(project, job)
+            for job in download_jobs
+        )
+    else:
+        title = "No download jobs found in the latest jobs of {}".format(project.path)
+        download_links = """<a href="{fetcher_jobs_url}" target="_blank" data-toggle="tooltip" data-placement="auto" title="{title}"><i class="fas fa-question-circle text-warning"></i></a>""".format(
+            fetcher_jobs_url=fetcher_jobs_url,
+            title=title,
+        )
+
+    if convert_jobs:
+        conversion_links = "".join(
+            format_job_link(project, job)
+            for job in convert_jobs
+        )
+    else:
+        title = "No conversion jobs found in the latest jobs of {}".format(project.path)
+        conversion_links = """<a href="{fetcher_jobs_url}" target="_blank" data-toggle="tooltip" data-placement="auto" title="{title}"><i class="fas fa-question-circle text-warning"></i></a>""".format(
+            fetcher_jobs_url=fetcher_jobs_url,
+            title=title,
+        )
+
     if index_jobs:
         indexation_links = "".join(
             format_job_link(importer_project, job)
@@ -172,15 +193,22 @@ def format_fetcher_tr(project, importer_project, provider_slug, pipeline_schedul
             index_jobs_url=index_jobs_url,
             title=title,
         )
+
     return """<tr>
-        <th scope="row">{provider_slug}</th>
+        <th scope="row">{provider_number}</th>
+        <th scope="row">
+            {provider_slug}
+            <a href="{git_link}" class="ml-2 small" target="_blank" title="View Git repository">git</a>
+        </th>
         <td>{pipeline_schedule_link}</td>
         <td>{download_links}</td>
         <td>{conversion_links}</td>
         <td>{indexation_links}</td>
     </tr>""".format(
         pipeline_schedule_link=pipeline_schedule_link,
+        provider_number=provider_number,
         provider_slug=provider_slug,
+        git_link="{}/{}".format(args.gitlab_base_url, project.path_with_namespace),
         download_links=download_links,
         conversion_links=conversion_links,
         indexation_links=indexation_links,
@@ -192,7 +220,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gitlab-base-url', default='https://git.nomics.world', help='base URL of GitLab instance')
     parser.add_argument('--fetchers', nargs='+', metavar='PROVIDER_SLUG',
-                        help='display dashboard for those fetchers only')
+                        help='generate dashboard for those fetchers only (space-separated)')
     parser.add_argument('--log', default='WARNING', help='level of logging messages')
     args = parser.parse_args()
 
@@ -209,6 +237,8 @@ def main():
     if args.gitlab_base_url.endswith('/'):
         args.gitlab_base_url = args.gitlab_base_url[:-1]
 
+    start_time = time.time()
+
     gl = gitlab.Gitlab(args.gitlab_base_url, private_token=os.environ.get('PRIVATE_TOKEN'), api_version=4)
     gl.auth()
 
@@ -219,7 +249,7 @@ def main():
         for group_project in dbnomics_fetchers_group.projects.list(order_by="name", sort="asc", all=True)
         # Skip other projects like "documentation".
         if group_project.name.endswith("-fetcher") and not group_project.name.startswith("dummy") and
-        (not args.fetchers or group_project.name[:len("-fetcher")] in args.fetchers)
+        (not args.fetchers or group_project.name[:-len("-fetcher")] in args.fetchers)
     ]
 
     # For each fetcher get its pipeline schedule.
@@ -259,7 +289,7 @@ def main():
     # Render the dashboard.
 
     print_html_dashboard(fetchers_projects, importer_project, pipeline_schedule_by_fetcher_id, jobs_by_fetcher_id,
-                         index_jobs_by_provider_slug)
+                         index_jobs_by_provider_slug, start_time)
 
     return 0
 
