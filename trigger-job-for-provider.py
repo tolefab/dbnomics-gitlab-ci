4de31a4fcf9e090d088727da80be94ee2db48325
Christophe Benz <christophe.benz@cepremap.org>
2018-12-28 20:06:04 +0100

    Implement "validate" action

--- trigger-job-for-provider.py
+++ trigger-job-for-provider.py
@@ -33,7 +33,6 @@ import sys
 
 import gitlab
 
-
 dbnomics_namespace = "dbnomics"
 dbnomics_fetchers_namespace = "dbnomics-fetchers"
 log = logging.getLogger(__name__)
@@ -42,7 +41,7 @@ log = logging.getLogger(__name__)
 def main():
     global args
     parser = argparse.ArgumentParser()
-    parser.add_argument('job_name', choices=['download', 'convert', 'index'], help='job name to trigger')
+    parser.add_argument('job_name', choices=['download', 'convert', 'index', 'validate'], help='job name to trigger')
     parser.add_argument('provider_slug', help='slug of the provider to configure')
     parser.add_argument('--gitlab-base-url', default='https://git.nomics.world', help='base URL of GitLab instance')
     parser.add_argument('--ref', default='master', help='ref of fetcher repo (branch name) on which to start the job')
@@ -66,11 +65,13 @@ def main():
     gl = gitlab.Gitlab(args.gitlab_base_url, private_token=os.environ.get('PRIVATE_TOKEN'), api_version=4)
     gl.auth()
 
+    dbnomics_group_url = args.gitlab_base_url + '/' + dbnomics_namespace
+    fetchers_group_url = args.gitlab_base_url + '/' + dbnomics_fetchers_namespace
+
     if args.job_name in {"download", "convert"}:
         fetcher_project = gl.projects.get("{}/{}-fetcher".format(dbnomics_fetchers_namespace, args.provider_slug))
         log.debug('fetcher project: {}'.format(fetcher_project))
 
-        fetchers_group_url = args.gitlab_base_url + '/' + dbnomics_fetchers_namespace
         fetcher_repo_url = '/'.join([fetchers_group_url, args.provider_slug + '-fetcher'])
 
         triggers = fetcher_project.triggers.list()
@@ -87,10 +88,29 @@ def main():
 
         fetcher_jobs_url = fetcher_repo_url + '/-/jobs'
         print('Check job: {}'.format(fetcher_jobs_url))
+    elif args.job_name == "validate":
+        data_model_repo_url = '/'.join([dbnomics_group_url, 'dbnomics-data-model'])
+
+        data_model_project = gl.projects.get("{}/dbnomics-data-model".format(dbnomics_namespace))
+        log.debug('data_model project: {}'.format(data_model_project))
+
+        triggers = data_model_project.triggers.list()
+        if len(triggers) != 1:
+            data_model_ci_settings_url = data_model_repo_url + '/settings/ci_cd'
+            log.error("Project should have one trigger, exit. See {}".format(data_model_ci_settings_url))
+            return 1
+        trigger = triggers[0]
+        log.debug('trigger of data_model repo fetched')
+
+        pipeline_variables = {'PROVIDER_SLUG': args.provider_slug}
+        data_model_project.trigger_pipeline(args.ref, trigger.token, pipeline_variables)
+        log.debug('pipeline triggered for ref {!r} with variables {!r}'.format(args.ref, pipeline_variables))
+
+        data_model_jobs_url = data_model_repo_url + '/-/jobs'
+        print('Check job: {}'.format(data_model_jobs_url))
     else:
         assert args.job_name == "index", args.job_name
 
-        dbnomics_group_url = args.gitlab_base_url + '/' + dbnomics_namespace
         importer_repo_url = '/'.join([dbnomics_group_url, 'dbnomics-importer'])
 
         importer_project = gl.projects.get("{}/dbnomics-importer".format(dbnomics_namespace))
