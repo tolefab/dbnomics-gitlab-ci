1ed8b929bcf2676bb8f1605f071571ac1bc1210b
Christophe Benz <christophe.benz@cepremap.org>
2019-01-09 11:48:20 +0100

    ls-providers.py: add --only-scheduled option

--- ls-providers.py
+++ ls-providers.py
@@ -28,24 +28,48 @@
 
 
 import argparse
+import os
 import subprocess
 import sys
 from pathlib import Path
 
 import gitlab
 
+GENERATED_OBJECTS_TAG = 'CI jobs'
+
 script_dir = Path(__file__).resolve().parent
+
+
+def iter_scheduled_fetcher_projects(gl, fetcher_projects):
+    for fetcher_group_project in fetcher_projects:
+        fetcher_project = gl.projects.get(fetcher_group_project.id)
+        provider_slug = fetcher_project.name.replace("-fetcher", "")
+        for pipeline_schedule in fetcher_project.pipelineschedules.list():
+            if pipeline_schedule.description == provider_slug + ' ' + GENERATED_OBJECTS_TAG and pipeline_schedule.active:
+                yield fetcher_project
+                break
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gitlab-url', default='https://git.nomics.world', help='base URL of GitLab instance')
+    parser.add_argument('--only-scheduled', action='store_true', help='display providers with an active scheduler')
     args = parser.parse_args()
 
-    gl = gitlab.Gitlab(url=args.gitlab_url)
+    gl = gitlab.Gitlab(url=args.gitlab_url, private_token=os.environ.get('PRIVATE_TOKEN'), api_version=4)
     dbnomics_fetchers_group = gl.groups.get('dbnomics-fetchers')
-    for project in dbnomics_fetchers_group.projects.list(order_by="name", sort="asc", all=True):
-        provider_slug = project.name.replace("-fetcher", "")
+
+    fetcher_projects = dbnomics_fetchers_group.projects.list(order_by="name", sort="asc", all=True)
+    if args.only_scheduled:
+        fetcher_projects = iter_scheduled_fetcher_projects(gl, fetcher_projects)
+
+    for fetcher_project in fetcher_projects:
+        if not fetcher_project.name.endswith("-fetcher"):
+            # Skip project named "management"
+            continue
+        if fetcher_project.name.startswith("dummy"):
+            continue
+        provider_slug = fetcher_project.name.replace("-fetcher", "")
         print(provider_slug)
 
     return 0
